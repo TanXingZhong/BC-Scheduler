5419607e21c557d429bcb4e59f9ca3fa72b3d746
tanxingzhong <zetter358@gmail.com>
2024-12-22 15:33:50 +0800

    Frontend calendar api call

---
+++ frontend/src/hooks/Calendar/useGetCalendar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const useGetCalendar = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchSchedule = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/schedules", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const json = await response.json();
+      if (!response.ok) {
+        setIsLoading(false);
+        setError(json.message || "Something went wrong");
+        return;
+      }
+      setIsLoading(false);
+      return json.rows;
+    } catch (error) {
+      console.log("Error getRoles", error);
+      setIsLoading(false);
+      setError("Error fetching all roles");
+    }
+  };
+
+  return { fetchSchedule, isLoading, error };
+};
--- frontend/src/pages/MyCalendar.jsx
+++ frontend/src/pages/MyCalendar.jsx
@@ -7,67 +7,82 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import { useEffect, useState, useCallback, useMemo } from "react";
+import { useGetCalendar } from "../hooks/Calendar/useGetCalendar";
+import { toSGTime } from "../../config/convertTimeToSGT";
 const localizer = momentLocalizer(moment);
 
-const calendarEvents = [
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date("2024-12-20T17:00:21.817Z"),
-    end: new Date("2024-12-20T23:00:21.817Z"),
-  },
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date("2024-12-20T17:00:21.817Z"),
-    end: new Date("2024-12-20T23:00:21.817Z"),
-  },
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date("2024-12-20T17:00:21.817Z"),
-    end: new Date("2024-12-20T23:00:21.817Z"),
-  },
-  {
-    title: "Math2",
-    allDay: false,
-    start: new Date("2024-12-20T23:00:21.817Z"),
-    end: new Date("2024-12-21T04:00:21.817Z"),
-  },
-  {
-    title: "Math22",
-    allDay: false,
-    start: new Date("2024-12-20T23:00:21.817Z"),
-    end: new Date("2024-12-21T04:00:21.817Z"),
-  },
-  {
-    title: "Math2",
-    allDay: false,
-    start: new Date("2024-12-20T23:00:21.817Z"),
-    end: new Date("2024-12-21T04:00:21.817Z"),
-  },
-  {
-    title: "Math2",
-    allDay: false,
-    start: new Date("2024-12-20T23:00:21.817Z"),
-    end: new Date("2024-12-21T04:00:21.817Z"),
-  },
-  {
-    title: "Math2",
-    allDay: false,
-    start: new Date("2024-12-20T23:00:21.817Z"),
-    end: new Date("2024-12-21T04:00:21.817Z"),
-  },
-];
-
 export default function MyCalendar() {
+  const events = [
+    {
+      id: 7,
+      title: "Lunch",
+      start: new Date(2024, 11, 12, 12, 0, 0, 0),
+      end: new Date(2024, 11, 12, 12, 12, 0, 0),
+      desc: "Power lunch",
+    },
+  ];
+  const [myEvents, setEvents] = useState([]);
+  const [schedule, setSchedule] = useState([]);
+  const onLoad = async () => {
+    try {
+      const val = await fetchSchedule();
+      console.log(val);
+      setSchedule(val);
+    } catch (err) {
+      console.error("Error loading schedules:", err);
+    }
+  };
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  const transformedDataArray = schedule.map((data) => {
+    const descString = ` Outlet: ${data.outlet_name} ${toSGTime(
+      data.start_time
+    )} - ${toSGTime(data.end_time)}, Vacancy: ${data.vacancy}`;
+    return {
+      title: descString,
+      start: new Date(data.start_time),
+      end: new Date(data.end_time),
+      desc: descString,
+    };
+  });
+
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedDateEvents, setSelectedDateEvents] = useState([]);
+  const { fetchSchedule, isLoading, error } = useGetCalendar();
 
   const handleShowMore = (events, date) => {
     setSelectedDateEvents(events);
     setModalOpen(true);
+  };
+
+  const handleSelectSlot = useCallback(
+    ({ start, end }) => {
+      const title = window.prompt("New Event name");
+      if (title) {
+        setEvents((prev) => [...prev, { start, end, title }]);
+      }
+    },
+    [setEvents]
+  );
+
+  const handleSelectEvent = useCallback(
+    (event) => window.alert(event.title),
+    []
+  );
+
+  const CustomEventWrapper = ({ event, children }) => {
+    return (
+      <div
+        style={{
+          border: "1px solid #007bff",
+        }}
+      >
+        <strong>{event.title}</strong>
+      </div>
+    );
   };
 
   return (
@@ -80,11 +95,17 @@ export default function MyCalendar() {
     >
       <Calendar
         localizer={localizer}
-        events={calendarEvents}
+        events={transformedDataArray}
         defaultView="month"
         style={{ height: "100%" }}
         views={["month"]}
         onShowMore={handleShowMore}
+        selectable
+        onSelectEvent={handleSelectEvent}
+        onSelectSlot={handleSelectSlot}
+        components={{
+          eventWrapper: CustomEventWrapper,
+        }}
       />
       <Dialog
         open={modalOpen}
