b58be26b8bcb3cfba231adc3bdfead2ecff27138
uylulu <uy.do.trong@gmail.com>
2024-12-23 10:05:37 +0700

    calendar feature

--- frontend/src/pages/MyCalendar.jsx
+++ frontend/src/pages/MyCalendar.jsx
@@ -5,6 +5,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useEffect, useState, useCallback } from "react";
 import { useGetCalendar } from "../hooks/Calendar/useGetCalendar";
+import { useGetNames } from "../hooks/Calendar/useGetNames";
 import { toSGTime } from "../../config/convertTimeToSGT";
 import { Grid2, Checkbox, FormControlLabel } from "@mui/material";
 
@@ -12,13 +13,18 @@ const localizer = momentLocalizer(moment);
 
 export default function MyCalendar() {
   const { fetchSchedule, isLoading, error } = useGetCalendar();
+  const { fetchNames } = useGetNames();
   const [schedule, setSchedule] = useState([]);
+  const [names, setNames] = useState([]);
 
   // Load schedule data when the component mounts
   const onLoad = async () => {
     try {
-      const val = await fetchSchedule();
-      setSchedule(val);
+      const schedules = await fetchSchedule();
+      const names = await fetchNames();
+
+      setSchedule(schedules);
+      setNames(names);
     } catch (err) {
       console.error("Error loading schedules:", err);
     }
@@ -27,22 +33,54 @@ export default function MyCalendar() {
   useEffect(() => {
     onLoad();
   }, []);
-
   // Transform the schedule data into a format that the Calendar accepts
-  const transformedDataArray = schedule.map((data) => {
-    console.log(data);
-    const descString = `${toSGTime(data.start_time)} - ${toSGTime(
-      data.end_time
-    )}, Vacancy: ${data.vacancy}`;
-    return {
-      title: descString,
-      start: new Date(data.start_time),
-      end: new Date(data.end_time),
-      desc: descString,
-      schedule_id: data.schedule_id, // Added for filtering
-      outlet_name: data.outlet_name, // Added for filtering
-    };
-  });
+  const transformedDataArray = schedule.reduce((acc, data) => {
+    // create new event objects based on vacancies and number of employees
+
+    const emptySlots = Array(data.vacancy).fill(
+      {
+        title: `${toSGTime(data.start_time)} - ${toSGTime(data.end_time)}, ${data.outlet_name}, EMPTY`,
+        start: new Date(data.start_time),
+        end: new Date(data.end_time),
+        desc: `Vacancy: ${data.vacancy}`,
+        schedule_id: data.schedule_id,
+        outlet_name: data.outlet_name,
+      }
+    );
+    
+    const filledSlots = data.employee_ids.body.map((id) => {
+      const employee = names.find((employee) => employee.id === id);
+
+      if(!employee) {
+        return {};
+      }
+
+      return {
+        title: `${toSGTime(data.start_time)} - ${toSGTime(data.end_time)}, ${data.outlet_name}, ${employee.name}`,
+        start: new Date(data.start_time),
+        end: new Date(data.end_time),
+        desc: `Vacancy: ${data.vacancy}`,
+        schedule_id: data.schedule_id,
+        outlet_name: data.outlet_name,
+      };
+    })
+    return [...acc, ...emptySlots, ...filledSlots];
+  }, []);
+
+  // const transformedDataArray = schedule.map((data) => {
+  //   console.log(data);
+  //   const descString = `${toSGTime(data.start_time)} - ${toSGTime(
+  //     data.end_time
+  //   )}, Vacancy: ${data.vacancy}`;
+  //   return {
+  //     title: descString,
+  //     start: new Date(data.start_time),
+  //     end: new Date(data.end_time),
+  //     desc: descString,
+  //     schedule_id: data.schedule_id, // Added for filtering
+  //     outlet_name: data.outlet_name, // Added for filtering
+  //   };
+  // });
 
   // Handle clicking on a day with events
   const handleShowMore = (events, date) => {
